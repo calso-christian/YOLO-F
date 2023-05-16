0159510305512758a0aadd149ae8b1a0e485df67
CorpuzKB <corpuzkio60@gmail.com>
2023-05-16 23:03:16 +0800

    Clean up

--- ArrayFoo/model.py
+++ ArrayFoo/model.py
@@ -1,20 +1,5 @@
-from IPython.display import set_matplotlib_formats
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 import cv2 as cv
 import tensorflow as tf
-from tensorflow import keras
-
-print(tf.__version__)
-
-architecture = {
-    'backbone': 'EfficientNetV2B1',
-    'SPPF_C': 480,
-    'Neck_C': [120, 240],
-    'Det_Blocks': 2,
-    'Head_Blocks': 1,
-    'Head_C': 128
-}
 
 config = {
     'LABELS': ['W', 'C'],
@@ -28,26 +13,18 @@ config = {
     'ANCHORS_shape': [3, 2],
     'PARAMS_GS_alpha': [1.5, 1.75, 2.0],
     'PARAMS_WH_power': [6.0, 4.0, 2.0],
-    'PARAMS_head_scale': [1.0, 1.5, 3.5],
-    'PARAMS_conf_scale': [0.02, 0.02, 0.02],
-    'RES_variations': [1088, 1024, 896, 832, 768, 704, 640]
 }
 
 colors = ['#FFA500', '#6A5ACD']
 
-def XYXY_to_YXYX(x):
-    x1, y1, x2, y2 = tf.split(x[..., :4], (1, 1, 1, 1), axis=-1)
-    return tf.concat([y1, x1, y2, x2], -1)
-
 def draw_predictions(image, pred_size, boxes, scores, labels, name=None):
-    col = [[60, 20, 220], [205, 90, 105]]
     image = tf.cast(tf.squeeze(image), tf.uint8)
     idx_non_zero = tf.where(scores)
     if idx_non_zero.shape[0] == 0:
         return image, None
-
+    
+    col = [[60, 20, 220], [205, 90, 105]]
     boxes = tf.cast((tf.gather_nd(boxes, idx_non_zero)) * pred_size, tf.int32)
-
     scores = tf.gather_nd(scores, idx_non_zero)
     labels = tf.gather_nd(labels, idx_non_zero)
     zeros = tf.zeros(image.shape)
@@ -60,14 +37,11 @@ def draw_predictions(image, pred_size, boxes, scores, labels, name=None):
             mask = tf.cast(cv.rectangle(zeros.numpy(), start, end, col[CLS], 4), tf.uint8)
             image = image * tf.cast((mask == 0), tf.uint8) + mask
             predictions.append((CLS, b[i]))
-
     return image, predictions
 
-
 class Output_Activation(tf.keras.layers.Layer):
-    def __init__(self, NUM_classes, anchors, GS_alpha, WH_power, training=False):
+    def __init__(self, NUM_classes, anchors, GS_alpha, WH_power):
         super().__init__()
-        self.training = training
         self.NUM_classes = NUM_classes
         self.anchors = anchors
         self.GS_alpha = GS_alpha
@@ -79,10 +53,8 @@ class Output_Activation(tf.keras.layers.Layer):
             x, (2, 2, 1, self.NUM_classes), axis=-1)
         box_xy = tf.sigmoid(box_xy)
         box_wh = ((2 * tf.sigmoid(box_wh)) ** self.WH_power) * self.anchors
-
-        if not self.training:
-            confidence = tf.sigmoid(confidence)
-            class_probs = tf.sigmoid(class_probs)
+        confidence = tf.sigmoid(confidence)
+        class_probs = tf.sigmoid(class_probs)
 
         grid = tf.meshgrid(tf.range(grid_size[0]), tf.range(grid_size[1]))
         grid = tf.stack(grid, axis=-1)[..., tf.newaxis, :]
@@ -93,7 +65,7 @@ class Output_Activation(tf.keras.layers.Layer):
         bbox = tf.concat([box_x1y1, box_x2y2], axis=-1)
         return bbox, confidence, class_probs
 
-class Post_Process(keras.layers.Layer):
+class Post_Process(tf.keras.layers.Layer):
     def __init__(self, NUM_classes, max_boxes=25, IoU_thresh=0.5, score_thresh=0.55):
         super().__init__()
         self.NUM_classes = NUM_classes
@@ -127,13 +99,11 @@ class Post_Process(keras.layers.Layer):
             max_total_size=self.max_boxes,
             iou_threshold=self.IoU_thresh,
             score_threshold=self.score_thresh)
-
         return boxes, scores, classes, valid_detections
 
     def call(self, x):
         x = self.NMS(x)
         return x
-
 
 @tf.function
 def predict(model, x):
@@ -141,9 +111,6 @@ def predict(model, x):
   y = [Output_Activation(config['NUM_classes'],
                         config['ANCHORS'][level],
                         config['PARAMS_GS_alpha'][level],
-                        config['PARAMS_WH_power'][level],
-                        training=False)(y[level]) for level in [0, 1, 2]]
-  y = Post_Process(config['NUM_classes'], 15, 0.25, 0.6)(y)
+                        config['PARAMS_WH_power'][level])(y[level]) for level in [0, 1, 2]]
+  y = Post_Process(config['NUM_classes'], 15, 0.2, 0.7)(y)
   return y
-
-model = tf.keras.models.load_model(r'ArrayFoo\weights\N\N_1')
